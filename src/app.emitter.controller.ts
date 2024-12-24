e4f8030f705ba41ed2b6ba5d5df0cc5310f55140
un4ckn0wl3z <admin@unknownclub.net>
2024-12-24 11:39:05 +0700

    feat: change sync emitter to async emitter

--- src/app.emitter.controller.ts
+++ src/app.emitter.controller.ts
@@ -9,10 +9,17 @@ export class AppEmitterController {
     private eventEmitter: EventEmitter2
 ) {}
 
+private emitAsync(session: string, request: any): Promise<void> {
+  return new Promise((resolve) => {
+      this.eventEmitter.emit(session, request);
+      resolve();
+  });
+}
+
   @EventPattern('test.request.topic')
     testTransformResponse(@Payload() request: any) {
     console.log("Received response from transformer message queue: ", request);
-    this.eventEmitter.emit(request.header.session, request);
+    this.emitAsync(request.header.session, request);
   }
 
 }
